f5c19ac48c706720456457e95b3cd11666cfa427
MaryPimenova <alla.pim-wolf@mail.ru>
2023-04-06 16:43:04 +0300

    Initial commit

---
+++ src/test/java/com/demoqa/DragAndDrop.java
@@ -0,0 +1,29 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+    @Test
+    void  productSearch() {
+        // Откройте https://the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        // Проверка верного открытия страницы
+        $(".example").shouldHave(text(("Drag and Drop")));
+        sleep(5000);
+        $("#column-a").shouldHave(exactText("A"));
+        $("#column-b").hover();
+
+        //actions().clickAndHold().moveByOffset(-200, 0).release().perform();
+        //$("#column-a").shouldHave(exactText("B"));
+
+        $("#column-a").dragAndDropTo($("#column-b"));
+        sleep(5000);
+        $("#column-a").shouldHave(exactText("B"));
+    }
+
+}
